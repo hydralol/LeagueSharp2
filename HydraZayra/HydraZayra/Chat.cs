da61a3e45b483f264fd25886fb5fb850db5db765
hydralol <deathsama@hotmail.fr>
2014-12-02 17:38:24 +0000

    Initial Release
    Initial Release For Zayra

---
+++ HydraZayra/HydraZayra/Chat.cs
@@ -0,0 +1,14 @@
+﻿using LeagueSharp;
+
+namespace HZaydra
+{
+	public static class Chat
+	{
+		public const string Basiccolor = HtmlColor.Red;
+
+		internal static void Print(string message, string color = Basiccolor)
+		{
+			Game.PrintChat("<font color='{0}'>{1}</font>", color, message);
+		}
+	}
+}
---
+++ HydraZayra/HydraZayra/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using LeagueSharp;
+using LeagueSharp.Common;
+using HydraZayra;
+
+namespace HydraZayra
+{
+	class Program
+	{
+		public const int LocalVersion = 1;
+        public const String Version = "1.0.*";
+
+		public static Champion Champion;
+		public static Menu Menu;
+		public static Orbwalking.Orbwalker Orbwalker;
+        public static Helper Helper;
+		private static void Main(string[] args)
+		{
+			CustomEvents.Game.OnGameLoad  += Game_OnGameLoad;
+		}
+
+		private static void Game_OnGameLoad(EventArgs args)
+		{
+			//AutoUpdater.InitializeUpdater();
+			Helper = new Helper();
+
+            Menu = new Menu("H Lucian", "Hydralucian_" + ObjectManager.Player.ChampionName, true);
+
+			var targetSelectorMenu = new Menu("Target Selector", "TargetSelector");
+			SimpleTs.AddToMenu(targetSelectorMenu);
+			Menu.AddSubMenu(targetSelectorMenu);
+			{
+				var orbwalking = Menu.AddSubMenu(new Menu("Orbwalking", "Orbwalking"));
+				Orbwalker = new Orbwalking.Orbwalker(orbwalking);
+				Menu.Item("FarmDelay").SetValue(new Slider(0, 0, 200));
+			}
+			var potionManager = new PotionManager();
+		
+			try
+			{
+                var handle = System.Activator.CreateInstance(null, "Hydralucian." + ObjectManager.Player.ChampionName);
+				Champion = (Champion) handle.Unwrap();
+			}
+			catch (Exception)
+			{
+			}
+					
+			Menu.AddToMainMenu();
+		}
+	}
+}
