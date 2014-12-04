1cec6deb03d2b167bc7e79fbee80bb71888f6773
hydralol <deathsama@hotmail.fr>
2014-12-04 00:09:40 +0000

    Morgana Update
    Morgana Update

---
+++ HydraMorgana/HydraMorgana/Chat.cs
@@ -0,0 +1,14 @@
+﻿using LeagueSharp;
+
+namespace HydraMorgana
+{
+	public static class Chat
+	{
+		public const string Basiccolor = HtmlColor.Red;
+
+		internal static void Print(string message, string color = Basiccolor)
+		{
+			Game.PrintChat("<font color='{0}'>{1}</font>", color, message);
+		}
+	}
+}
